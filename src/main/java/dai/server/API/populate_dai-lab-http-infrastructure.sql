c9cf2109b38a66d31ea290343c0485b1b3d66421
LucasHSN <lucas.hussain@heig-vd.ch>
2023-12-14 11:23:08 +0100

    Add database sql files + Insert data

---
+++ src/main/java/dai/server/API/populate_dai-lab-http-infrastructure.sql
@@ -0,0 +1,20 @@
+--
+-- Database: family
+-- Johan Mikami, Hussain Lucas
+-- populate_dai-lab-http-infrastructure.sql
+--
+
+SET SEARCH_PATH TO family;
+
+-- --------------------------------------------------------
+--
+-- Dumping data for table person
+--
+INSERT INTO person VALUES('John', 'Doe', '1981-06-19', '+41 111 22 33', 1);
+INSERT INTO person VALUES('Mark', 'Grayson', '2000-01-05', '+41 666 23 31', 2);
+INSERT INTO person VALUES('Kalye', 'Huston', '1987-09-11', '+41 321 45 67', 3);
+INSERT INTO person VALUES('Bob', 'Huston', '1988-11-09', '+41 123 54 76', 3);
+
+INSERT INTO address VALUES(1, 'Rue de la Gare 1', '1000', 'Lausanne', 'Switzerland');
+INSERT INTO address VALUES(2, 'Avenue du chapiteau 5', '1400', 'Yverdon-les-Bains', 'Switzerland');
+INSERT INTO address VALUES(3, 'Ruelle Rousseau 12', '2114', 'Fleurier', 'Switzerland');
