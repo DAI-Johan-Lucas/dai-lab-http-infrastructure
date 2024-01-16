e26427db36b4ebe0f576a190b0d66ae9ea16bb80
LucasHSN <lucas.hussain@heig-vd.ch>
2024-01-16 10:31:08 +0100

    Remove unnecessary comments

--- src/main/java/server/API/sql/populate_dai-lab-http-infrastructure.sql
+++ src/main/java/server/API/sql/populate_dai-lab-http-infrastructure.sql
@@ -18,7 +18,3 @@ INSERT INTO person VALUES(1, 'John', 'Doe', '1981-06-19', '+41 111 22 33', 1);
 INSERT INTO person VALUES(2, 'Mark', 'Grayson', '2000-01-05', '+41 666 23 31', 2);
 INSERT INTO person VALUES(3, 'Kalye', 'Huston', '1987-09-11', '+41 321 45 67', 3);
 INSERT INTO person VALUES(4, 'Bob', 'Huston', '1988-11-09', '+41 123 54 76', 3);
-
-UPDATE person SET firstname = 'slt', lastname = 'mec', birthdate = '1981-06-19', phone = '+41 666 66 66',
-                  fk_address = '1'
-              WHERE id = '22';
